9a0cb7773b20d8305a62ae3fbca99b79d4dc77df
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-03-21 17:11:59 +0200

    Update UserRole.cs

--- HotelReservationManager/Models/UserRole.cs
+++ HotelReservationManager/Models/UserRole.cs
@@ -1,12 +1,17 @@
- public class UserRole
-    {
-        [Required]
-        [ForeignKey("User")]
-        public int UserId { get; set; }
-        public User User { get; set; }
+using HotelReservationManager.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
 
-        [Required]
-        [StringLength(10)]
-        public string Role { get; set; } // "Admin" or "User"
-    }
+public class UserRole
+{
+    [Key, Column(Order = 0)]
+    public int UserId { get; set; }
+
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; }
+
+    [Key, Column(Order = 1)]
+    [Required]
+    [StringLength(10)]
+    public string Role { get; set; } // "Admin" or "User"
 }
