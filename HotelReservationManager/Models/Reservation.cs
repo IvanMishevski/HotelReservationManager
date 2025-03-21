5321754de1bb187a04024081de4a916334d71d08
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-03-21 17:10:25 +0200

    Update Reservation.cs

--- HotelReservationManager/Models/Reservation.cs
+++ HotelReservationManager/Models/Reservation.cs
@@ -1,36 +1,40 @@
+using HotelReservationManager.Models;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
 public class Reservation
-    {
-        [Key]
-        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-        public int Id { get; set; }
+{
+    [Key]
+    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+    public int Id { get; set; }
 
-        [Required]
-        public int RoomId { get; set; }
+    [Required]
+    public int RoomId { get; set; }
 
-        [ForeignKey("RoomId")]
-        public virtual Room Room { get; set; }
+    [ForeignKey("RoomId")]
+    public virtual Room Room { get; set; }
 
-        [Required]
-        public int UserId { get; set; }
+    [Required]
+    public int UserId { get; set; }
 
-        [ForeignKey("UserId")]
-        public virtual User User { get; set; }
+    [ForeignKey("UserId")]
+    public virtual User User { get; set; }
 
-        [Required]
-        public DateTime CheckInDate { get; set; }
+    [Required]
+    public DateTime CheckInDate { get; set; }
 
-        [Required]
-        public DateTime CheckOutDate { get; set; }
+    [Required]
+    public DateTime CheckOutDate { get; set; }
 
-        [Required]
-        public bool BreakfastIncluded { get; set; }
+    [Required]
+    public bool BreakfastIncluded { get; set; }
 
-        [Required]
-        public bool AllInclusive { get; set; }
+    [Required]
+    public bool AllInclusive { get; set; }
 
-        [Required]
-        [Column(TypeName = "decimal(18,2)")]
-        public decimal TotalPrice { get; set; }
+    [Required]
+    [Column(TypeName = "decimal(18,2)")]
+    public decimal TotalPrice { get; set; }
 
-        public virtual ICollection<ReservationClient> ReservationClients { get; set; }
-    }
+    public virtual ICollection<ReservationClient> ReservationClients { get; set; }
+}
