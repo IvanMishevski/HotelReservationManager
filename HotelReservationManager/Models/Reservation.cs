421254fa45b8bfd3464dc8bf51983c723e389a2d
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-03-22 10:19:35 +0200

    Update Reservation.cs

--- HotelReservationManager/Models/Reservation.cs
+++ HotelReservationManager/Models/Reservation.cs
@@ -1,40 +1,50 @@
-using HotelReservationManager.Models;
-using System.ComponentModel.DataAnnotations.Schema;
+using System;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
-public class Reservation
+namespace HotelReservationManager.Models
 {
-    [Key]
-    [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
-    public int Id { get; set; }
+    public class Reservation
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int Id { get; set; }
 
-    [Required]
-    public int RoomId { get; set; }
+        [Required]
+        public int RoomId { get; set; }
 
-    [ForeignKey("RoomId")]
-    public virtual Room Room { get; set; }
+        [ForeignKey("RoomId")]
+        public Room Room { get; set; }
 
-    [Required]
-    public int UserId { get; set; }
+        [Required]
+        public int UserId { get; set; }
 
-    [ForeignKey("UserId")]
-    public virtual User User { get; set; }
+        [ForeignKey("UserId")]
+        public User User { get; set; }
 
-    [Required]
-    public DateTime CheckInDate { get; set; }
+        public string ClientList { get; set; } // List of clients (can be stored as a string or a separate table)
 
-    [Required]
-    public DateTime CheckOutDate { get; set; }
+        [Required]
+        public DateTime CheckInDate { get; set; }
 
-    [Required]
-    public bool BreakfastIncluded { get; set; }
+        [Required]
+        [CustomDateRange]
+        public DateTime CheckOutDate { get; set; }
 
-    [Required]
-    public bool AllInclusive { get; set; }
+        public bool IsBreakfastIncluded { get; set; }
 
-    [Required]
-    [Column(TypeName = "decimal(18,2)")]
-    public decimal TotalPrice { get; set; }
+        public bool IsAllInclusive { get; set; }
 
-    public virtual ICollection<ReservationClient> ReservationClients { get; set; }
+        [Required]
+        public decimal AmountDue { get; set; }
+    }
+
+    public class CustomDateRangeAttribute : ValidationAttribute
+    {
+        public override bool IsValid(object value)
+        {
+            DateTime checkOutDate = (DateTime)value;
+            return checkOutDate > DateTime.Now; // Ensure checkout date is in the future
+        }
+    }
 }
