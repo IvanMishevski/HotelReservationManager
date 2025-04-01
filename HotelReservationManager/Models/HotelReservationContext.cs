eed4e8dca6cd9b9a348675c721fa093075e026ac
aylin-s06 <123801066+aylin-s06@users.noreply.github.com>
2025-04-01 10:03:18 +0300

    Added Images to Room DBSet

--- HotelReservationManager/Models/HotelReservationContext.cs
+++ HotelReservationManager/Models/HotelReservationContext.cs
@@ -7,7 +7,6 @@ namespace HotelReservationManager.Models
 {
     public class HotelReservationContext : DbContext
     {
-        
         public HotelReservationContext(DbContextOptions<HotelReservationContext> options)
             : base(options)
         { }
@@ -60,16 +59,16 @@ namespace HotelReservationManager.Models
                 .HasForeignKey(ur => ur.UserId);
 
             modelBuilder.Entity<User>()
-               .Property(u => u.DateOfHire)
-               .HasDefaultValueSql("GETDATE()");
+                .Property(u => u.DateOfHire)
+                .HasDefaultValueSql("GETDATE()");
 
             // Seed Data
 
             modelBuilder.Entity<Room>().HasData(
-                new Room { Id = 1, RoomNumber = 101, Capacity = 2, RoomType = "Double Room", IsAvailable = true, PriceForAdult = 50, PriceForChild = 30 },
-                new Room { Id = 2, RoomNumber = 102, Capacity = 4, RoomType = "Apartment", IsAvailable = true, PriceForAdult = 120, PriceForChild = 60 },
-                new Room { Id = 3, RoomNumber = 103, Capacity = 2, RoomType = "Room with Double Bed", IsAvailable = true, PriceForAdult = 75, PriceForChild = 40 },
-                new Room { Id = 4, RoomNumber = 104, Capacity = 1, RoomType = "Penthouse", IsAvailable = true, PriceForAdult = 200, PriceForChild = 100 }
+                new Room { Id = 1, RoomNumber = 101, Capacity = 2, RoomType = "Double Room", IsAvailable = true, PriceForAdult = 50, PriceForChild = 30, ImageUrl = "https://media.hotel7dublin.com/image/upload/f_auto,g_auto,c_auto,w_3840,q_auto/v1708595213/Uploads/Hotel7/Cosy_Room_Hero_643fdf08b9.jpg" },
+                new Room { Id = 2, RoomNumber = 102, Capacity = 4, RoomType = "Apartment", IsAvailable = true, PriceForAdult = 120, PriceForChild = 60, ImageUrl = "https://dynamic-media-cdn.tripadvisor.com/media/photo-o/2c/b0/b6/2b/apartment-hotels.jpg?w=1200&h=-1&s=1" },
+                new Room { Id = 3, RoomNumber = 103, Capacity = 2, RoomType = "Room with Double Bed", IsAvailable = true, PriceForAdult = 75, PriceForChild = 40, ImageUrl = "https://image-tc.galaxy.tf/wijpeg-bxdv8c6ji6oftcroue33x7hfl/double-duble_standard.jpg?crop=122%2C0%2C1757%2C1318" },
+                new Room { Id = 4, RoomNumber = 104, Capacity = 1, RoomType = "Penthouse", IsAvailable = true, PriceForAdult = 200, PriceForChild = 100, ImageUrl = "https://www.paranych.com/uploads/benefits-penthouse-living-main-image.png" }
             );
 
             modelBuilder.Entity<User>().HasData(
